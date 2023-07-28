24ecd7fa642fff28cbde7a3f785bd169b50c5a73
Groinkb <benjamin@mossu.ch>
2023-07-28 22:14:54 +0200

    news

--- printf.c
+++ printf.c
@@ -1,33 +1,87 @@
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 
-int percent(const char *format, ...)
+/**
+ * _putchar - Write a character to the standard output (file descriptor 1).
+ *
+ * @c: The character to be written.
+ *
+ * Return: On success, 1 is returned. On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _printf - Print formatted output with variable arguments.
+ *
+ * This function prints formatted output with variable arguments. It supports
+ * replacing '%d' or '%i' with an integer value, '%s' with a null-terminated string,
+ * '%c' with a character, and '%%' with a literal '%'.
+ *
+ * @format: The formatted string to print.
+ *
+ * Return: The number of characters printed (excluding the null terminator).
+ */
+int _printf(const char *format, ...)
 {
 	va_list args;
+	int printed_chars = 0;
+	const char *str;
+	char *p;
+
 	va_start(args, format);
 
-	int printed_chars = 0;
-
-	while (*format)
+	for (str = format; str && *str != '\0'; str++)
 	{
-		if (*format == '%')
+		if (*str == '%')
 		{
-			format++;
-			if (*format == '%')
+			switch (*(str + 1))
 			{
-				write(1, format, 1);
-				printed_chars++;
+			case 'd':
+			case 'i':
+			{
+				int value = va_arg(args, int);
+				char buffer[20];
+				snprintf(buffer, sizeof(buffer), "%d", value);
+				for (p = buffer; *p != '\0'; p++)
+				{
+					printed_chars += _putchar(*p);
+				}
+				break;
 			}
+			case 's':
+			{
+				char *value = va_arg(args, char *);
+				for (p = value; *p != '\0'; p++)
+				{
+					printed_chars += _putchar(*p);
+				}
+				break;
+			}
+			case 'c':
+			{
+				char value = (char)va_arg(args, int);
+				printed_chars += _putchar(value);
+				break;
+			}
+			case '%':
+				printed_chars += _putchar('%');
+				break;
+			default:
+				break;
+			}
+			str++;
 		}
 		else
 		{
-			write(1, format, 1);
-			printed_chars++;
+			printed_chars += _putchar(*str);
 		}
-		format++;
 	}
 
 	va_end(args);
 
-	return printed_chars;
-	}
+	return (printed_chars);
+}
