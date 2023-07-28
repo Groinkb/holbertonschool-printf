e59b7667822180047b4771a3ec285d1f50847759
Groinkb <you@example.com>
2023-07-28 14:57:22 +0200

    main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int c (const char *format, ...);
+int d (const char *format, ...);
+int i (const char *format, ...);
+int s (const char *format, ...);
+int percent(const char *format, ...);
+int _printf(const char *format, ...);
+
+#endif
