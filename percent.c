509d956b1c1be67d7bca387252b6eaacfca89296
Groinkb <benjamin@mossu.ch>
2023-07-29 22:54:12 +0200

    Function description for function percent

--- percent.c
+++ percent.c
@@ -2,6 +2,23 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+/**
+ * percent - Custom print function that prints formatted output to the standard output,
+ *           handling double percent signs "%%" and printing other characters as is.
+ *
+ * @format: A pointer to a format string containing the text to be printed, along with
+ *          optional format specifiers.
+ * @...   : Variable arguments list, corresponding to the format specifiers in the
+ *          format string. Currently, no format specifiers are supported.
+ *
+ * Return: The number of characters printed to the standard output.
+ *
+ * Note: This function behaves like a simpler version of printf and is mainly intended
+ *       for demonstration purposes or when advanced formatting is not required. It
+ *       supports the "%%" format specifier to print a single percent sign. All other
+ *       characters in the format string are printed as is.
+ */
+
 int percent(const char *format, ...)
 {
 	int printed_chars = 0;
